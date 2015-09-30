ecd7bb8dd826407ac3c3533dd11781d6fd7ccef7
Mayas Haddad <mayashaddad32@gmail.com>
2015-09-30 17:09:29 +0200

    uppercased menu labels and rearranged colors and adopted roboto font

--- public/js/src/components/Menu.js
+++ public/js/src/components/Menu.js
@@ -4,7 +4,8 @@ import MenuItem from './MenuItem';
 var menuStyle = {
 	width: 10 + '%',
     position: 'fixed',
-    marginTop: 10 + 'em'
+    marginTop: 10 + 'em',
+    fontFamily: 'Roboto'
 };
 
 export default class Menu extends React.Component {
@@ -12,12 +13,12 @@ export default class Menu extends React.Component {
 	render() {
 		return (
 			<div style={menuStyle}>
-				<MenuItem label={"Latest"} textColor={'white'} color={"#F44336"} /><br/>
-				<MenuItem label={"Technical"} textColor={'white'} color={"#3F51B5"} /><br/>
-				<MenuItem label={"Methodologies"} textColor={'white'} color={"#9C27B0"} /><br/>
-				<MenuItem label={"UX"} textColor={'white'} color={"#F57F17"} /><br/>
-				<MenuItem label={"Broad"} color={"#AEEA00"} /><br/>
-				<MenuItem label={"About Me"} color={"#C6FF00"} /><br/>
+				<MenuItem label={"LATEST"} textColor={'white'} color={"#F44336"} /><br/>
+				<MenuItem label={"TECHNOLOGIES"} textColor={'white'} color={"#3F51B5"} /><br/>
+				<MenuItem label={"METHODOLOGIES"} textColor={'white'} color={"#9C27B0"} /><br/>
+				<MenuItem label={"UX"} color={"#FFEB3B"} /><br/>
+				<MenuItem label={"BROAD"} textColor={'white'} color={"#F57F17"} /><br/>
+				<MenuItem label={"ABOUT ME"} textColor={'white'} color={"#4CAF50"} /><br/>
 			</div>);
 	}
 }
