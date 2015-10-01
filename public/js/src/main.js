514c405bbf43df6f12dc0932b99985797f343c63
Mayas Haddad <mayashaddad32@gmail.com>
2015-10-01 17:54:14 +0200

    now using react-router to route to different pages

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,3 +1,24 @@
-var React = require('react');
-var Blog = require('./components/Blog');
-React.render(<Blog/>, document.getElementById('app'));
+var React = require('react'),
+Latest = require('./components/pages/Latest'),
+Technologies = require('./components/pages/Technologies'),
+Methodologies = require('./components/pages/Methodologies'),
+UX = require('./components/pages/UX'),
+Broad = require('./components/pages/Broad'),
+AboutMe = require('./components/pages/AboutMe');
+import { Router, Route } from 'react-router';
+import URLs from './constants/URLs.json';
+
+var app = document.getElementById('app');
+
+React.render((
+		<Router>
+		    <Route path="/" component={Latest}/>
+		    <Route path={URLs.latestPageUrl} component={Latest}/>
+		    <Route path={URLs.technologiesPageUrl} component={Technologies}/>
+		    <Route path={URLs.methodologiesPageUrl} component={Methodologies}/>
+		    <Route path={URLs.uxPageUrl} component={UX}/>
+		    <Route path={URLs.broadPageUrl} component={Broad}/>
+		    <Route path={URLs.aboutMePageUrl} component={AboutMe}/>
+		</Router>
+	), 
+app);
