b86ad441aeee3c503ac0d8295a6029af037f79f2
Mayas Haddad <mayashaddad32@gmail.com>
2015-10-01 17:58:39 +0200

    now using labels and colors from json const files

--- public/js/src/components/Menu.js
+++ public/js/src/components/Menu.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import MenuItem from './MenuItem';
-
+import ThemeColors from '../../../styles/themeColors.json';
+import URLs from '../constants/URLs.json';
+import Labels from '../constants/Labels.json';
 var menuStyle = {
 	width: 10 + '%',
     position: 'fixed',
@@ -9,16 +11,43 @@ var menuStyle = {
 };
 
 export default class Menu extends React.Component {
-	
 	render() {
 		return (
 			<div style={menuStyle}>
-				<MenuItem label={"LATEST"} textColor={'white'} color={"#F44336"} /><br/>
-				<MenuItem label={"TECHNOLOGIES"} textColor={'white'} color={"#3F51B5"} /><br/>
-				<MenuItem label={"METHODOLOGIES"} textColor={'white'} color={"#9C27B0"} /><br/>
-				<MenuItem label={"UX"} color={"#FFEB3B"} /><br/>
-				<MenuItem label={"BROAD"} textColor={'white'} color={"#F57F17"} /><br/>
-				<MenuItem label={"ABOUT ME"} textColor={'white'} color={"#4CAF50"} /><br/>
+				<MenuItem 
+					pageLink={URLs.latestPageUrl}
+					label={Labels.latestLabel} 
+					textColor={'white'} 
+					color={ThemeColors.latestThemeColor} />
+				<br/>
+				<MenuItem
+					pageLink={URLs.technologiesPageUrl}
+					label={Labels.technologiesLabel} 
+					textColor={'white'} 
+					color={ThemeColors.technologiesThemeColor} />
+				<br/>
+				<MenuItem 
+					pageLink={URLs.methodologiesPageUrl}
+					label={Labels.methodologiesLabel} 
+					textColor={'white'} 
+					color={ThemeColors.methodologiesThemeColor} />
+				<br/>
+				<MenuItem 
+					pageLink={URLs.uxPageUrl}
+					label={Labels.uxLabel} 
+					color={ThemeColors.uxThemeColor} />
+				<br/>
+				<MenuItem 
+					pageLink={URLs.broadPageUrl}
+					label={Labels.broadLabel} 
+					textColor={'white'} 
+					color={ThemeColors.broadThemeColor} />
+				<br/>
+				<MenuItem 
+					pageLink={URLs.aboutMePageUrl}
+					label={Labels.aboutMeLabel} 
+					textColor={'white'} 
+					color={ThemeColors.aboutMeThemeColor} /><br/>
 			</div>);
 	}
 }
