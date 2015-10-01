41c8ae48a9f496f7e6d4c45fe3f99d81585a6584
Mayas Haddad <mayashaddad32@gmail.com>
2015-10-01 18:02:52 +0200

    added the urls constants, removed Blog.js and added the paper body

--- public/js/src/components/MenuItem.js
+++ public/js/src/components/MenuItem.js
@@ -5,16 +5,27 @@ export default class MenuItem extends React.Component {
 	constructor(props) {
         super(props);
         this.props = props;
+        this.linkTo = this.linkTo.bind(this);
+	}
+
+	linkTo() {
+		location.href = "/#" + this.props.pageLink;
 	}
 
 	render() {
-		return <div style={{
-			backgroundColor: this.props.color,
-			color: this.props.textColor,
-			padding: 0.5 + 'em',
-			textAlign: 'center',
-			borderRadius: 1 + 'px',
-			cursor: 'pointer'
-		}} >{this.props.label}</div>;
+		return (
+			<div 
+				onClick={this.linkTo}
+				style={{
+					backgroundColor: this.props.color,
+					color: this.props.textColor,
+					padding: 0.5 + 'em',
+					textAlign: 'center',
+					borderRadius: 1 + 'px',
+					cursor: 'pointer'
+				}} >
+				{this.props.label}
+			</div>
+		);
 	}
 }
--- public/js/src/components/PaperBody.js
+++ public/js/src/components/PaperBody.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class PaperHeader extends React.Component {
+export default class PaperBody extends React.Component {
 	
 	constructor(props) {
         super(props);
