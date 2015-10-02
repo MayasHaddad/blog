b74a518296193fea766ca0e4b86032fb5abb16c6
Mayas Haddad <mayas_91@hotmail.com>
2015-10-02 17:21:26 +0200

    index route now fetches all the posts

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,13 +1,37 @@
 var keystone = require('keystone');
+var async = require('async');
 
 exports = module.exports = function(req, res) {
 	
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 	
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
+	// Init locals
+	locals.section = 'latest';
+	locals.data = {
+		posts: [],
+		categories: []
+	};
+	
+	
+	// Load the posts
+	view.on('init', function(next) {
+		
+		var q = keystone.list('Post').paginate({
+				page: req.query.page || 1,
+				perPage: 10,
+				maxPages: 10
+			})
+			.where('state', 'published')
+			.sort('-publishedDate')
+			.populate('author categories');
+
+		q.exec(function(err, results) {
+			locals.data.posts = results;
+			next(err);
+		});
+		
+	});
 	
 	// Render the view
 	view.render('index');
